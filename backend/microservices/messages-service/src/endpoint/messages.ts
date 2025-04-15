fde5fdf2d2896abbeda477de2ad7c54d9a870501
Mathéo <159580780+Math-Vov13@users.noreply.github.com>
2025-04-15 15:07:21 +0200

    Messages ms (#8)
    * SOCKET.TS UPDATE .1
    
    * Endpoint/Messages.TS Update + Socket.TS Update (V.1.2)
    
    ---------
    
    Co-authored-by: Kya Shift <joao-gabriel.marques-dinis@efrei.net>

--- backend/microservices/messages-service/src/endpoint/messages.ts
+++ backend/microservices/messages-service/src/endpoint/messages.ts
@@ -1,74 +1,39 @@
 import { Router, Request, Response } from 'express';
-import { verifyToken } from '../security/jwt';
-import { getAllMessages, addMessage, getMessagesByMarkID } from '../model/db_messages';
+import { getAllMessages, getMessagesByMarkID } from '../dbtest/db_messages'; // getAllMessages : Optionel (l. 24)
 
 const router = Router();
 
 /**
  * GET /messages
- * Récupère tous les messages ou filtre par coordonnées
+ * Récupère tous les messages ou ceux liés à un markerID
  * Accessible sans authentification
  */
-router.get('/', (req: Request, res: Response) => {
+router.get('/', async (req: Request, res: Response) => {
+  const { markerID } = req.query;
+
   try {
-    const { markerID } = req.query;
-
-    // MarkerID est obligatoire ! Tu peux d'ailleurs utiliser mon middleware pour ne pas avoir à vérifier sa valeur et renvoyer un message d'erreur si non spécifié
-    if (markerID) {
-      const messages = getMessagesByMarkID(markerID as string);
-      res.status(200).json(messages);
-      return;
+    if (typeof markerID === "string" && markerID.trim() !== "") { // Si y a un markerID
+      const messages = await getMessagesByMarkID(markerID);
+      res.status(200).json({
+        markerID,
+        length: messages.length,
+        messages,
+      })
+      return ;
     }
-
-    const messages = getAllMessages();
+    /*         Logique, obtenir tout les messages (Optionnel)
+    const messages = await getAllMessages();
     res.status(200).json({
-      "length": messages.length,
-      "messages": messages,
-      "connected": true,
-    });
+      length: messages.length,
+      messages,
+    })
     return;
-
+    */
   } catch (error) {
     console.error('Erreur lors de la récupération des messages:', error);
-    res.status(500).json({ message: 'Erreur serveur' });
-    return
+    res.status(500).json({ message: 'Erreur serveur' })
+    return ;
   }
 });
-
-/**
- * POST /messages
- * Ajoute un nouveau message (nécessite authentification)
- */
-// router.post('/', verifyToken, (req: Request, res: Response) => {
-//   try {
-//     const { text, markerID } = req.body;
-
-//     if (!text) {
-//       res.status(400).json({ message: 'Le contenu du message est requis' });
-//       return;
-//     }
-
-//     const user = req.user?.username;
-
-//     if (!user) {
-//       res.status(401).json({ message: 'Utilisateur non authentifié' });
-//       return;
-//     }
-
-//     const newMessage = addMessage({
-//       user,
-//       text,
-//       date: new Date().toISOString(),
-//       markerID: markerID
-//     });
-
-//     res.status(201).json(newMessage);
-//     return
-//   } catch (error) {
-//     console.error('Erreur lors de l\'ajout du message:', error);
-//     res.status(500).json({ message: 'Erreur serveur' });
-//     return;
-//   }
-// });
 
 export default router;
