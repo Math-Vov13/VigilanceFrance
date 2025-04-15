a56a5d76ef18fa32f8a852cdd59aec00d82e3fb3
Math_13 <MathVov.91@outlook.fr>
2025-04-15 23:04:44 +0200

    +/- || Stronger input Schema

--- backend/microservices/maps-service/src/schemas/marker_sc.ts
+++ backend/microservices/maps-service/src/schemas/marker_sc.ts
@@ -20,12 +20,12 @@ export const MarkerDB = z.object({
 })
 
 export const IssueCreate = z.object({
-    type: z.string().min(6),
-    title: z.string().min(6),
-    description: z.string().min(6),
+    type: z.enum(["mineur", "moyen", "majeur", "critique"]),
+    title: z.string().min(6).max(30),
+    description: z.string().min(6).max(100),
 
-    severity: z.string().min(6),
-    location: z.string().min(6),
+    severity: z.enum(["accident", "inondation", "incendie", "vol", "agression", "manifestation", "panne", "pollution", "autre"]),
+    location: z.string().min(6).max(80),
     coordinates: z.object({
         lat: z.number().positive().or( z.number().negative() ),
         lng: z.number().positive().or( z.number().negative() )
